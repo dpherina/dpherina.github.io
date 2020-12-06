b28cfc4d667d60e0c7bb0f435b2c46c0e38f74b9
dpherina <dpherina@gmail.com>
2020-12-06 15:40:16 -0500

    Updates

---
+++ precache-manifest.dec14e36e932e429e4a6c0ceb11ab0c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581afe4ceb0fb364d9b5918b745fa105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "280c5b31f930237a026c",
+    "url": "/static/css/main.2de9f67c.chunk.css"
+  },
+  {
+    "revision": "4c5e868bd9da28e52687",
+    "url": "/static/js/2.59215c93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.59215c93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280c5b31f930237a026c",
+    "url": "/static/js/main.54e3a98b.chunk.js"
+  },
+  {
+    "revision": "c49622d2dece6fea9b56",
+    "url": "/static/js/runtime-main.1b2a38a0.js"
+  }
+]);
